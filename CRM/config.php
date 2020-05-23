8a65be015e60b402d43ad21592f279b4d0625d68
Sizar Azad <sizarazad86@gmail.com>
2020-05-23 10:10:08 +0300

    update

---
+++ CRM/config.php
@@ -0,0 +1,9 @@
+<?php
+$msqlhost = 'localhost'; //Hostname of database
+$msqluser = 'root';                   //User of database
+$msqlpass = '7727984430Azad';           //Password of user/database
+$msqldbnm = 'cydia';              //Database name
+mysql_connect ($msqlhost, $msqluser, $msqlpass)       or die(mysql_error());
+
+mysql_select_db ($msqldbnm)                           or die(mysql_error());
+?>
