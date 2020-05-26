f23dcde0d71b78600e80b380cf8bff2abafeda1e
Sizar Azad <sizarazad86@gmail.com>
2020-05-26 10:47:46 +0300

    update

--- description.js
+++ description.js
@@ -22,7 +22,7 @@ function toogleTheme() {
 }
 
 function loadPackageInfo() {
-	var urlSelfParts = window.location.href.split('description.html?id=');
+	var urlSelfParts = window.location.href.split('description.html?p=');
 	var current_url_path = window.location.href.slice(0, window.location.href.lastIndexOf('/'));
 	var form_url = current_url_path + "/packageInfo/" + urlSelfParts[1];
 	if (navigator.userAgent.search(/Cydia/) == -1) {
@@ -93,7 +93,7 @@ function openSection(sectionName) {
 		var urlOpen = "cydia://package/" + dicNow.package;
 		if (navigator.userAgent.search(/Cydia/) == -1) {
 			var current_url_path = window.location.href.slice(0, window.location.href.lastIndexOf('/'));
-			urlOpen = current_url_path + "/description.html?id=" + dicNow.package;
+			urlOpen = current_url_path + "/description.html?p=" + dicNow.package;
 		}
 		sectionContent += "<li class=\"has-icon\"><a href='" + urlOpen + "' target='_blank' role=\"button\"><img style=\"border-radius: 20%;\" href=\"" + encodeURI(sectionName) + ".png\" alt=\"\" srcset=\"" + encodeURI(sectionName) + ".png 2x, " + encodeURI(sectionName) + ".png 3x\" class=\"icon\"/><label>" + dicNow.name + " v" + dicNow.version + "</label></a></li>";
 	}
@@ -119,7 +119,7 @@ function getPackageWithID(pkg_id) {
 }
 
 function loadPackageDetail() {
-	var packageID = window.location.href.split('description.html?id=')[1];
+	var packageID = window.location.href.split('description.html?p=')[1];
 	var packageInfo = getPackageWithID(packageID);
 	if (packageInfo != null) {
 		$("#pkg_").show();
@@ -143,7 +143,7 @@ function loadRecentUpdates() {
 		var urlOpen = "cydia://package/" + allPackages[dicNow].package;
 		if (navigator.userAgent.search(/Cydia/) == -1) {
 			var current_url_path = window.location.href.slice(0, window.location.href.lastIndexOf('/'));
-			urlOpen = current_url_path + "/description.html?id=" + allPackages[dicNow].package;
+			urlOpen = current_url_path + "/description.html?p=" + allPackages[dicNow].package;
 		}
 		htmlnews += "<li class=\"has-icon\"><a href='" + urlOpen + "' target='_blank' role=\"button\"><img style=\"border-radius: 20%;\" href=\"" + encodeURI(allPackages[dicNow].section) + ".png\" alt=\"\" srcset=\"" + encodeURI(allPackages[dicNow].section) + ".png 2x, " + encodeURI(allPackages[dicNow].section) + ".png 3x\" class=\"icon\"/><label>" + allPackages[dicNow].name + " v" + allPackages[dicNow].version + "</label></a></li>";
 	}
