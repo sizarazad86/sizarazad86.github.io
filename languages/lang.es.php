1224d401540855dc6f2b87ae2fce5365a8cbf457
Sizar Azad <sizarazad86@gmail.com>
2020-05-26 04:47:01 +0300

    update

---
+++ languages/lang.es.php
@@ -0,0 +1,75 @@
+<?php
+/* 
+------------------
+Language: Spanish
+------------------
+*/
+
+$lang = array();
+
+$lang['PAGE_TITLE'] = 'iQPHONE';
+$lang['HEADER_TITLE'] = 'iQPHONE';
+$lang['SITE_NAME'] = 'iQPHONE';
+$lang['HEADING'] = 'iQPHONE';
+$lang['HEADER_ABOUT'] = 'Información';
+$lang['HEADER_RELOAD'] ='Refrescar';
+$lang['LOADING_SCREEN'] = 'Cargando...';
+$lang['WELCOME_HEADER'] = 'Bienvenido a iQPHONE';
+$lang['LANG'] = 'Lenguaje';
+$lang['BY'] = 'By';
+$lang['WORKS'] = 'Funciona Bien';
+$lang['NEED_AIRPLANE'] = 'Necesita Truco Modo Avión';
+$lang['NEED_LOOPHOLE'] = 'Necesita Truco LoopHole';
+$lang['DOESNT_WORK'] = 'No Funciona';
+$lang['UNKNOWN'] = 'Talvez Funciona';
+$lang['APPLE_UPDATES'] = 'Actuizaciones Apple Beta';
+$lang['ICON_BACK'] = 'icon-back';
+
+// Files
+
+$lang['LANG_AIRPLANE'] = 'airplane.html';
+$lang['LANG_LOOPHOLE'] = 'loophole.html';
+$lang['LANG_APPLEID'] = 'appleid.html';
+$lang['LANG_CHANGELOG'] = 'changelog.html';
+$lang['RTL'] = 'css/framework7.ios.min.css';
+$lang['STYLE'] = 'css/my-app.min.css';
+
+// about.php
+
+$lang['BACK'] = 'Atras';
+$lang['FROM_TUTU'] = 'Algunos de los links son de TuTu Helper';
+$lang['CREATOR'] = 'Creador';
+$lang['TEAM_MEMBERS'] = 'Miembros del Equipo';
+$lang['DARK_MODE'] = 'Modo Oscuro';
+$lang['AIRPLANE_TRICK'] = 'Truco Modo Avión';
+$lang['LOOPHOLE_TRICK'] = 'Truco Loophole';
+$lang['CHANGELOG'] = 'Vitacora';
+$lang['FOLLOW_US'] = 'Siguenos';
+$lang['THANKS_TO'] = 'Gracias a';
+
+// Menu
+
+$lang['HOME'] = 'Destacados';
+$lang['APPS'] = 'Apps';
+$lang['GAMES'] = 'Juegos';
+$lang['PLUS_APPS'] = 'Apps ++';
+$lang['HACKED_GAMES'] = 'Juegos Hackeados';
+$lang['DUPLICATED_APPS'] = 'Apps Duplicadas';
+$lang['MORE'] = 'Más';
+$lang['MORE_HACKED'] = 'Juegos Hackeados';
+$lang['MORE_APPS'] = 'Apps y Juegos';
+$lang['MORE_DUPLICATED'] = 'Apps Duplicadas';
+$lang['MORE_APPLEID'] = 'Cuentas AppleID';
+$lang['MORE_WALLPAPER'] = 'Fondos de Pantalla';
+$lang['MORE_TUT'] = 'Tutoriales';
+$lang['MORE_CYDIA'] = 'Cydia';
+$lang['MORE_REPO'] = 'Fuente en Cydia';
+$lang['MORE_SUGGEST'] = 'Sugiere una App para Agregar';
+$lang['MORE_UNABLE'] = 'No puedes Instalar? Avisanos!';
+$lang['SEE_ALL'] = 'Ver Todo';
+$lang['NEWEST'] = 'Lo más Nuevo';
+$lang['BADGE_BETA'] = 'Beta';
+$lang['BADGE_SOON'] = 'Proximamente';
+$lang['BADGE_NEW'] = 'Nuevo';
+$lang['CHEVRON'] = '_right';
+?>
---
+++ languages/lang.fr.php
@@ -0,0 +1,75 @@
+<?php
+/* 
+------------------
+Language: French
+------------------
+*/
+
+$lang = array();
+
+$lang['PAGE_TITLE'] = "iQPHONE";
+$lang['HEADER_TITLE'] = "iQPHONE";
+$lang['SITE_NAME'] = "iQPHONE";
+$lang['HEADING'] = "iQPHONE";
+$lang['HEADER_ABOUT'] = "À propos";
+$lang['HEADER_RELOAD'] = "Actualiser";
+$lang['LOADING_SCREEN'] = "Chargement...";
+$lang['WELCOME_HEADER'] = "Bienvenue sur iQPHONE";
+$lang['LANG'] = "Langues";
+$lang['BY'] = "Par";
+$lang['WORKS'] = "Fonctionne Bien";
+$lang['NEED_AIRPLANE'] = "Besoin du Truc 'Mode Avion'";
+$lang['NEED_LOOPHOLE'] = "Besoin du Truc de Temps";
+$lang['DOESNT_WORK'] = "Ne Fonctionne Pas";
+$lang['UNKNOWN'] = "Ne Fonctionne Peut-Être Pas";
+$lang['APPLE_UPDATES'] = "Béta d'iOS";
+$lang['ICON_BACK'] = "icon-back";
+
+// Files
+
+$lang['LANG_AIRPLANE'] = "airplane.html";
+$lang['LANG_LOOPHOLE'] = "loophole.html";
+$lang['LANG_APPLEID'] = "appleid.html";
+$lang['LANG_CHANGELOG'] = "changelog.html";
+$lang['RTL'] = "css/framework7.ios.min.css";
+$lang['STYLE'] = "css/my-app.min.css";
+
+// about.php
+
+$lang['BACK'] = "Retour";
+$lang['FROM_TUTU'] = "Certains Liens Viennent de Tutu Helper";
+$lang['CREATOR'] = "Créateur";
+$lang['TEAM_MEMBERS'] = "Membres de l'Équipe";
+$lang['DARK_MODE'] = "Mode Sombre";
+$lang['AIRPLANE_TRICK'] = "Truc 'Mode Avion'";
+$lang['LOOPHOLE_TRICK'] = "Truc de Temps";
+$lang['CHANGELOG'] = "Notes de Mise à Jour";
+$lang['FOLLOW_US'] = "Suivez-Nous";
+$lang['THANKS_TO'] = "Grâce à";
+
+// Menu
+
+$lang['HOME'] = "En Vedette";
+$lang['APPS'] = "Apps";
+$lang['GAMES'] = "Jeux";
+$lang['PLUS_APPS'] = "Apps ++";
+$lang['HACKED_GAMES'] = "Jeux Piratés";
+$lang['DUPLICATED_APPS'] = "Apps Dupliquées";
+$lang['MORE'] = "Plus";
+$lang['MORE_HACKED'] = "Jeux Piratés";
+$lang['MORE_APPS'] = "Apps et Jeux";
+$lang['MORE_DUPLICATED'] = "Apps Dupliquées";
+$lang['MORE_APPLEID'] = "Identifiant Apple";
+$lang['MORE_WALLPAPER'] = "Fond d'Écran";
+$lang['MORE_TUT'] = "Tutoriels";
+$lang['MORE_CYDIA'] = "Cydia";
+$lang['MORE_REPO'] = "Source Cydia";
+$lang['MORE_SUGGEST'] = "Suggérer Des Applications";
+$lang['MORE_UNABLE'] = "Impossible d'Installer? Laisser-nous savoir";
+$lang['SEE_ALL'] = "Tout Voir";
+$lang['NEWEST'] = "Nouveauté";
+$lang['BADGE_BETA'] = "Béta";
+$lang['BADGE_SOON'] = "Bientôt";
+$lang['BADGE_NEW'] = "Nouveau";
+$lang['CHEVRON'] = "_right";
+?>
